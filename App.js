e372489f0954d591a78d3edced4abc68b2c93a6a
mubeenaTechknocks <67516058+mubeenaTechknocks@users.noreply.github.com>
2020-09-26 23:14:37 +0530

    navigation

--- App.js
+++ App.js
@@ -79,328 +79,328 @@ const Tab = createBottomTabNavigator();
 
 function TabNavigation({navigation}) {
   return (
-    <Tab.Navigator
-      screenOptions={({route}) => ({
-        tabBarIcon: ({focused, color, size}) => {
-          if (route.name === 'Home') {
-            return (
-              <MaterialCommunityIcons
-                name={focused ? 'home-circle' : 'home-circle'}
-                size={size}
-                color={color}
-                style={{marginTop: 3}}
-              />
-            );
-          } else if (route.name === 'Category') {
-            return (
-              <MaterialCommunityIcons
-                name={
-                  focused
-                    ? 'format-list-bulleted-square'
-                    : 'format-list-checkbox'
-                }
-                size={size}
-                color={color}
-                style={{marginTop: 3}}
-              />
-            );
-          } else if (route.name === 'Offers') {
-            return (
-              <MaterialCommunityIcons
-                name={
-                  focused
-                    ? 'currency-usd-circle-outline'
-                    : 'currency-usd-circle-outline'
-                }
-                size={size}
-                color={color}
-                style={{marginTop: 3}}
-              />
-            );
-          } else if (route.name === 'Search') {
-            return (
-              <Ionicons
-                name={focused ? 'search-outline' : 'search-outline'}
-                size={size}
-                color={color}
-                style={{marginTop: 3}}
-              />
-            );
-          } else if (route.name === 'Basket') {
-            return (
-              <MaterialCommunityIcons
-                name={focused ? 'cart' : 'cart'}
-                size={size}
-                color={color}
-                style={{marginTop: 3}}
-              />
-            );
-          }
-        },
-      })}
-      tabBarOptions={{
-        activeTintColor: '#537e2c',
-        inactiveTintColor: 'grey',
-      }}>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Category" component={Category} />
-      <Tab.Screen name="Search" component={Search} />
-      <Tab.Screen name="Offers" component={Offers} />
-      <Tab.Screen name="Basket" component={Basket} />
-    </Tab.Navigator>
+    <HomeStack.Navigator initialRouteName="Home">
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Home"
+        component={HomeScreen}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Fruits"
+        component={Fruits}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Productd"
+        component={Productd}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="HomeScreen"
+        component={HomeScreen}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Product"
+        component={Product}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Display"
+        component={Display}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Snacks"
+        component={Snacks}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Foodgrains"
+        component={Foodgrains}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Bakery"
+        component={Bakery}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Eggs"
+        component={Eggs}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Beauty"
+        component={Beauty}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Kitchen"
+        component={Kitchen}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Beverages"
+        component={Beverages}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Cleaning"
+        component={Cleaning}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Gourmet"
+        component={Gourmet}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Baby"
+        component={Baby}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Address"
+        component={Address}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Delivery"
+        component={Delivery}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name=" Loginscreen"
+        component={Loginscreen}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Exotic"
+        component={Exotic}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Flower"
+        component={Flower}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Fresh_fruits"
+        component={Fresh_fruits}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Fruitall"
+        component={Fruitall}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Herbs"
+        component={Herbs}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Organics"
+        component={Organics}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Seasonal"
+        component={Seasonal}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Sprouts"
+        component={Sprouts}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Vegetablesf"
+        component={Vegetablesf}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Filter"
+        component={Filter}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Atta"
+        component={Atta}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Rice"
+        component={Rice}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Oil"
+        component={Oil}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Dals"
+        component={Dals}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Organic_staples"
+        component={Organic_staples}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Spices"
+        component={Spices}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Dry_Fruits"
+        component={Dry_Fruits}
+      />
+      {/* <HomeStack.Screen
+      options={{headerShown: false}}
+      name="Spices"
+      component={Spices}
+    /> */}
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Sugar"
+        component={Sugar}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Allfoodgrains"
+        component={Allfoodgrains}
+      />
+
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Loginscreen"
+        component={Loginscreen}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Milk"
+        component={Milk}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Curd"
+        component={Curd}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Paneer"
+        component={Paneer}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Cheese"
+        component={Cheese}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Cake"
+        component={Cake}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Bread"
+        component={Bread}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Cookie"
+        component={Cookie}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Goumet"
+        component={Goumet}
+      />
+      <HomeStack.Screen
+        options={{headerShown: false}}
+        name="Bsnack"
+        component={Bsnack}
+      />
+    </HomeStack.Navigator>
   );
 }
 export default function App() {
   return (
     <NavigationContainer>
-      <HomeStack.Navigator initialRouteName="Home">
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Home"
-          component={TabNavigation}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Fruits"
-          component={Fruits}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Productd"
-          component={Productd}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="HomeScreen"
-          component={HomeScreen}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Product"
-          component={Product}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Display"
-          component={Display}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Snacks"
-          component={Snacks}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Foodgrains"
-          component={Foodgrains}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Bakery"
-          component={Bakery}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Eggs"
-          component={Eggs}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Beauty"
-          component={Beauty}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Kitchen"
-          component={Kitchen}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Beverages"
-          component={Beverages}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Cleaning"
-          component={Cleaning}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Gourmet"
-          component={Gourmet}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Baby"
-          component={Baby}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Address"
-          component={Address}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Delivery"
-          component={Delivery}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name=" Loginscreen"
-          component={Loginscreen}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Exotic"
-          component={Exotic}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Flower"
-          component={Flower}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Fresh_fruits"
-          component={Fresh_fruits}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Fruitall"
-          component={Fruitall}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Herbs"
-          component={Herbs}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Organics"
-          component={Organics}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Seasonal"
-          component={Seasonal}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Sprouts"
-          component={Sprouts}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Vegetablesf"
-          component={Vegetablesf}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Filter"
-          component={Filter}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Atta"
-          component={Atta}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Rice"
-          component={Rice}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Oil"
-          component={Oil}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Dals"
-          component={Dals}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Organic_staples"
-          component={Organic_staples}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Spices"
-          component={Spices}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Dry_Fruits"
-          component={Dry_Fruits}
-        />
-        {/* <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Spices"
-          component={Spices}
-        /> */}
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Sugar"
-          component={Sugar}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Allfoodgrains"
-          component={Allfoodgrains}
-        />
-
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Loginscreen"
-          component={Loginscreen}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Milk"
-          component={Milk}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Curd"
-          component={Curd}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Paneer"
-          component={Paneer}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Cheese"
-          component={Cheese}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Cake"
-          component={Cake}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Bread"
-          component={Bread}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Cookie"
-          component={Cookie}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Goumet"
-          component={Goumet}
-        />
-        <HomeStack.Screen
-          options={{headerShown: false}}
-          name="Bsnack"
-          component={Bsnack}
-        />
-      </HomeStack.Navigator>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            if (route.name === 'Home') {
+              return (
+                <MaterialCommunityIcons
+                  name={focused ? 'home-circle' : 'home-circle'}
+                  size={size}
+                  color={color}
+                  style={{marginTop: 3}}
+                />
+              );
+            } else if (route.name === 'Category') {
+              return (
+                <MaterialCommunityIcons
+                  name={
+                    focused
+                      ? 'format-list-bulleted-square'
+                      : 'format-list-checkbox'
+                  }
+                  size={size}
+                  color={color}
+                  style={{marginTop: 3}}
+                />
+              );
+            } else if (route.name === 'Offers') {
+              return (
+                <MaterialCommunityIcons
+                  name={
+                    focused
+                      ? 'currency-usd-circle-outline'
+                      : 'currency-usd-circle-outline'
+                  }
+                  size={size}
+                  color={color}
+                  style={{marginTop: 3}}
+                />
+              );
+            } else if (route.name === 'Search') {
+              return (
+                <Ionicons
+                  name={focused ? 'search-outline' : 'search-outline'}
+                  size={size}
+                  color={color}
+                  style={{marginTop: 3}}
+                />
+              );
+            } else if (route.name === 'Basket') {
+              return (
+                <MaterialCommunityIcons
+                  name={focused ? 'cart' : 'cart'}
+                  size={size}
+                  color={color}
+                  style={{marginTop: 3}}
+                />
+              );
+            }
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: '#537e2c',
+          inactiveTintColor: 'grey',
+        }}>
+        <Tab.Screen name="Home" component={TabNavigation} />
+        <Tab.Screen name="Category" component={Category} />
+        <Tab.Screen name="Search" component={Search} />
+        <Tab.Screen name="Offers" component={Offers} />
+        <Tab.Screen name="Basket" component={Basket} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
